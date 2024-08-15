74d7a636d4f0e4c59abb8d88ee517b9205d8cc84
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-15 22:48:49 +0530

    Create 11.String.js

---
+++ 01.basics/11.String.js
@@ -0,0 +1,19 @@
+// we will use modern syntax for string 
+const name = "shubham"
+const repoCount = 50
+
+console.log(name + repoCount + " Value"); // old concatinate method
+
+// / New Method / 
+// back ticks  i.e string Interpolation 
+
+console.log(`My name ${name} and my age is ${repoCount} `);
+
+
+// *****************************************************
+
+const gameName =  new String('shubhampragatistr')
+
+console.log(typeof gameName);
+
+
