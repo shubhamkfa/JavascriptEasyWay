5d0893e722c6ddaa707405ea956c87e9e3dd965b
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-16 23:36:33 +0530

    Update 06.ConversionOperational.js

--- 01.basics/06.ConversionOperational.js
+++ 01.basics/06.ConversionOperational.js
@@ -32,3 +32,64 @@ console.log(temp)
 
 let tempNum = Number (temp)
 console.log(typeof tempNum)
+console.log(tempNum)
+
+
+let temp = "undefined String"
+
+console.log(typeof temp)
+console.log(temp)
+
+let tempstr = Number (temp)
+console.log(typeof tempstr)
+console.log(tempstr)
+
+
+let isLoggedIn = 1
+
+console.log(isLoggedIn)
+console.log(typeof isLoggedIn)
+
+let isBoolean =  Boolean(isLoggedIn)
+console.log(typeof isBoolean)
+
+
+let isLoggedB = false
+
+console.log(isLoggedB)
+console.log(typeof isLoggedB)
+
+let isBooleanN =  Number(isLoggedB)
+console.log(typeof isBooleanN)
+console.log(isBooleanN)
+
+
+let isLoggedB = ""
+
+console.log(isLoggedB)
+console.log(typeof isLoggedB)
+
+let isBooleanN =  Boolean(isLoggedB)
+console.log(typeof isBooleanN)
+console.log(isBooleanN)
+
+/*************Notes  
+"33"=> 33
+"33abc"=> NaN
+true => 1 ; false =>
+
+boolean
+    1=> true , 0= >false
+    " " => false , "text"=> true
+
+    */
+
+
+let someNumber = 33;
+let someString = String (someNumber);
+console.log(typeof someString)
+console.log(someString);
+
+
+
+
