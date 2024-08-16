c64b908bc8950b6310f4463da9c80a34b5d79814
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-16 23:35:54 +0530

    Update 06.ConversionOperational.js

--- 01.basics/06.ConversionOperational.js
+++ 01.basics/06.ConversionOperational.js
@@ -14,3 +14,21 @@ console.log(typeof numString)
 let num1 = Number (numString) // NaN Not a Number 
 console.log (num1) //NaN
 console.log(typeof num1) //Number
+let temp = null 
+
+console.log(typeof temp)
+console.log(temp)
+
+let tempNum = Number (temp)
+console.log(typeof tempNum)
+console.log(tempNum)
+
+
+
+let temp = undefined
+
+console.log(typeof temp)
+console.log(temp)
+
+let tempNum = Number (temp)
+console.log(typeof tempNum)
