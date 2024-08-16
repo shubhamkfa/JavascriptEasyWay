fbfa4116ceebec00beb1f36538003f67dfb72cae
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-16 23:35:16 +0530

    Create 06.ConversionOperational.js

---
+++ 01.basics/06.ConversionOperational.js
@@ -0,0 +1,16 @@
+/*
+let score = 33;
+let scoreString = "36"
+console.log(typeof score) // number
+console.log(typeof(scoreString))//string
+
+let valueInNUmber = Number (scoreString)
+console.log(typeof valueInNUmber) // converted to Number 
+
+
+
+let numString = "123abc"
+console.log(typeof numString)
+let num1 = Number (numString) // NaN Not a Number 
+console.log (num1) //NaN
+console.log(typeof num1) //Number
