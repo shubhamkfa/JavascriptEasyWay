95c3c1afbb1fe38fbb6164f6db15c9fdf267d708
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-17 21:12:06 +0530

    Create 11.1 StringMethodPractice.js

---
+++ 01.basics/11.1 StringMethodPractice.js
@@ -0,0 +1,26 @@
+/* 1. JavaScript String Length
+The length property returns the length of a string:
+
+*/
+let text = "shubhamShinde";
+console.log(text.length);
+
+
+
+
+/*Extracting String Characters
+There are 4 methods for extracting string characters:
+
+The at(position) Method
+The charAt(position) Method
+The charCodeAt(position) Method
+Using property access [] like in arrays
+
+JavaScript String charAt()
+The charAt() method returns the character at a specified index (position) in a string:
+*/
+
+console.log(text.charAt(3))
+
+
+
