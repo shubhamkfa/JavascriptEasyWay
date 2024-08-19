53a73560a76657448d396d253247657c80187c99
Shubham <152170670+shubhamkfa@users.noreply.github.com>
2024-08-19 10:17:23 +0530

    Update 11.1 StringMethodPractice.js

--- 01.basics/11.1 StringMethodPractice.js
+++ 01.basics/11.1 StringMethodPractice.js
@@ -3,7 +3,7 @@ The length property returns the length of a string:
 
 */
 let text = "shubhamShinde";
-console.log(text.length);
+// console.log(text.length);
 
 
 
@@ -20,7 +20,24 @@ JavaScript String charAt()
 The charAt() method returns the character at a specified index (position) in a string:
 */
 
-console.log(text.charAt(3))
+// console.log(text.charAt(3))
 
+// The trim() method removes whitespace from both sides of a string:
+let strName ='     Pragati     shubham    shinde   '
+// console.log(strName.trim());
 
+// The trimStart() method works like trim(), but removes whitespace only from the start of a string.
 
+// console.log(strName.trimStart())
+
+// The trimEnd() method works like trim(), but removes whitespace only from the end of a string.
+
+// console.log(strName.trimEnd());
+
+// The padStart() method pads a string from the start.
+
+// It pads a string with another string (multiple times) until it reaches a given length
+
+// console.log(strName.padStart(4,1));
+
+console.log(strName.substr());
